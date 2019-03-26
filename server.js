3ef794f9edce08658701c7a30c0aea48761d3b5f
Maxime Salomon <maxime@croissant.io>
2019-03-26 11:03:32 +0100

    projects route + CRUD

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const projectRouter = require('./routes/project-router')
+const projectRouter = require('./routes/project-router')
 
 const server = express();
 const parser = express.json();
 server.use(parser);
 
-// server.use('/api/projects', projectRouter)
+server.use('/api/projects', projectRouter)
 
 module.exports = server;
