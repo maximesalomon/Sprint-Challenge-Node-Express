590aa2ff0c5c56650cc4dcfbccf972fea4d3b8bb
Maxime Salomon <maxime@croissant.io>
2019-03-26 10:40:28 +0100

    npm script server + start

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const projectRouter = require('./routes/project-router')
+
+const server = express();
+const parser = express.json();
+server.use(parser);
+
+// server.use('/api/projects', projectRouter)
+
+module.exports = server;
